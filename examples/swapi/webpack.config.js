17f2484eb3c9ad964ae13ad50085045ab24ce3a8
Alain Armand <idkjs@users.noreply.github.com>
2018-04-24 10:11:07 +0200

    add mode key to clean up console errors in browser
    less noise while checking out the repo

--- examples/swapi/webpack.config.js
+++ examples/swapi/webpack.config.js
@@ -1,7 +1,9 @@
 const path = require('path');
+const isProd = process.env.NODE_ENV === "production";
 
 module.exports = {
   entry: './src/index.bs.js',
+  mode: isProd ? "production" : "development",
   output: {
     path: path.join(__dirname, "build"),
     filename: 'bundle.js',
